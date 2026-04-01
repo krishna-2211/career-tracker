fd46d27b52b24214e2a695a260fd42720bcce1e8
krishna-2211 <kpanchal2211@gmail.com>
2026-03-31 21:04:23 -0500

    error resolution

--- app/main.py
+++ app/main.py
@@ -17,7 +17,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # React dev server
+    allow_origins=["http://localhost:5173"],  # React dev server
     allow_methods=["*"],
     allow_headers=["*"],
 )
